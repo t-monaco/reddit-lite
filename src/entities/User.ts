918c828c46ac342a65592804908a4cea5c840a6f
Tomas Monaco <tomas.monaco@wundermanthompson.com>
2021-07-29 11:51:51 -0300

    Added user entity

---
+++ src/entities/User.ts
@@ -0,0 +1,26 @@
+import { Field } from 'type-graphql';
+import { ObjectType } from 'type-graphql';
+import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
+
+@ObjectType()
+@Entity()
+export class User {
+    @Field()
+    @PrimaryKey()
+    id!: number;
+
+    @Field()
+    @Property()
+    createdAt: Date = new Date();
+
+    @Field()
+    @Property({ onUpdate: () => new Date() })
+    updatedAt: Date = new Date();
+
+    @Field()
+    @Property({unique: true})
+    username!: string;
+
+    @Property({unique: true})
+    password!: string;
+}
