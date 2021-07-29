f58808491c5288698874a09ed34ae175b0e0e043
Tomas Monaco <tomas.monaco@wundermanthompson.com>
2021-07-29 11:52:48 -0300

    Include resolver and entity

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import { __prod__ } from './constants';
 import mikroConfig from './mikro-orm.config';
 import { HelloResolver } from './resolvers/hello';
 import { PostResolver } from './resolvers/post';
+import { UserResolver } from './resolvers/user';
 
 const main = async () => {
     const orm = await MikroORM.init(mikroConfig);
@@ -16,16 +17,13 @@ const main = async () => {
 
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
-            resolvers: [HelloResolver, PostResolver],
+            resolvers: [HelloResolver, PostResolver, UserResolver],
             validate: false,
         }),
         context: () => ({ em: orm.em }),
     });
     await apolloServer.start();
     apolloServer.applyMiddleware({ app });
-    // app.get('/', (_, res) => {
-    //     res.send('HELLLOOOOO');
-    // });
 
     app.listen(4000, () => {
         console.log('Listening on localhost:4000');
