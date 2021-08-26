bcbd9fe3139309543c4bb115e7ff1cd806cea8eb
Tomás Mónaco <monacotomas99@gmail.com>
2021-08-25 23:01:48 -0300

    Implemented layout + create post link

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,17 +1,23 @@
 import { withUrqlClient } from "next-urql"
+import React from "react"
+import Layout from "../components/Layout"
 import NavBar from "../components/Navbar"
 import { usePostsQuery } from "../generated/graphql"
 import { createUrqlClient } from "../utils/createUrqlClient"
+import NextLink from 'next/link'
+import { Link } from "@chakra-ui/react"
 
 const Index = () => {
     const [{data}] = usePostsQuery()
 
     return (
-        <>
-            <NavBar />
+        <Layout>
+            <NextLink href='create-post'>
+                <Link>Create Post</Link>
+            </NextLink>
             <h1>HELLO WOOLRD</h1>
             {!data ? null : data.posts.map(p => <h4>{p.title}</h4>)}
-        </>
+        </Layout >
     )
 }
 
