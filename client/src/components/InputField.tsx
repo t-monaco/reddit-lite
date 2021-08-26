e9ffe0460bdeab12ce010624d2f30aa8aaf255e1
Tomás Mónaco <monacotomas99@gmail.com>
2021-08-25 23:03:44 -0300

    Added textarea option

--- client/src/components/InputField.tsx
+++ client/src/components/InputField.tsx
@@ -1,18 +1,23 @@
 import React, { InputHTMLAttributes } from 'react'
-import { FormControl, FormErrorMessage, FormLabel, Input } from '@chakra-ui/react'
+import { FormControl, FormErrorMessage, FormLabel, Input, Textarea } from '@chakra-ui/react'
 import { useField } from 'formik'
 
 type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
     label: string
     name: string
+    textarea?: boolean
 }
 
-const InputField: React.FC<InputFieldProps> = ({ label, size: _, ...props }) => {
+const InputField: React.FC<InputFieldProps> = ({ label, textarea, size: _, ...props }) => {
+    let InputOrTextarea = Input
+    if (textarea){
+        InputOrTextarea = Textarea
+    }
     const [field, { error }] = useField(props)
     return (
         <FormControl isInvalid={!!error}>
             <FormLabel htmlFor={field.name}>{label}</FormLabel>
-            <Input {...field} {...props} id={field.name} />
+            <InputOrTextarea {...field} {...props} id={field.name} />
             {error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
         </FormControl>
     )
