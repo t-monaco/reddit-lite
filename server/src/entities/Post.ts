c09af90102c5dacfa79d7be6581e8efc6e8ce606
Tomás Mónaco <monacotomas99@gmail.com>
2021-08-28 23:48:58 -0300

    Added voteStatus to Post Entity

--- server/src/entities/Post.ts
+++ server/src/entities/Post.ts
@@ -1,4 +1,4 @@
-import { Field } from 'type-graphql';
+import { Field, Int } from 'type-graphql';
 import { ObjectType } from 'type-graphql';
 import {
     BaseEntity,
@@ -31,6 +31,9 @@ export class Post extends BaseEntity {
     @Field()
     @Column({ type: 'int', default: 0 })
     points!: number;
+
+    @Field(() => Int, { nullable: true })
+    voteStatus: number; // 1 or -1 or null
 
     @Field()
     @Column()
